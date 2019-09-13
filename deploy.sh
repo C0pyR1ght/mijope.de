36f1fdbc59ca509a7b6ce18bee13e49231425362
Michael <michaeljohannpeter@gmail.com>
2019-09-13 16:17:10 +0200

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" != 1 ]; then
+  echo "Wrong number of arguments"
+  exit 1
+fi
+
+function copy {
+  rsync -avhr --delete -e ssh $1 rootknecht:$2
+}
+
+
+hugo
+case $1 in
+    dev )
+        echo "Deploying to test.rootknecht.net"
+        copy ./public/ /var/www/test/
+        ssh rootknecht chown -R knecht:www-data /var/www/test
+        ;;
+    prod )
+        echo "Deploying to mijope.de"
+        scp -r ./public/* rootknecht:/var/www/mijope
+        ssh rootknecht chown -R knecht:www-data /var/www/mijope
+        ;;
+esac
